e17b901afbf3cfb61e23416b01b72e60c82ac400
whywesmurfing <raymond.doan94@gmail.com>
2021-07-23 17:24:35 +1000

    renamed file to reflect the database name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const companyDb = require("./db/company");
+const companyDb = require("./db/sws");
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
