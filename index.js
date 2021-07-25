bfc6bd69f7f1a5ac49bdacc0ce1f46d9c071e285
whywesmurfing <raymond.doan94@gmail.com>
2021-07-25 13:22:01 +1000

    fixed cors issue

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors") 
+
 const app = express();
 const port = process.env.PORT || 4000;
 
 const companyDb = require("./db/sws");
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
